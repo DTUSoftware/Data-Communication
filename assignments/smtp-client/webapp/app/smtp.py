8a4dd2dfcb0d53e432f605e95d4b0f5c942ad86b
HeroGamers <15278940+HeroGamers@users.noreply.github.com>
2022-03-13 21:31:22 +0100

    Initial rework and SSL adding, haven't tested on a secure server

--- assignments/smtp-client/webapp/app/smtp.py
+++ assignments/smtp-client/webapp/app/smtp.py
@@ -1,10 +1,217 @@
 import os
 import random
-import socket as _socket
+import socket
+import ssl
 
 ADDRESS = os.environ.get("MAILSERVER_ADDRESS", "localhost")
 PORT = int(os.environ.get("MAILSERVER_PORT", "2525"))
+USE_SSL = True if os.environ.get("USE_SSL", "False").lower() == "true" else False
 BOUNDARY_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'()+_,-./:=?"
+
+
+class Connection:
+    def __init__(self):
+        self.sock = None
+        self.ssock = None
+        self.scontext = None
+
+        self.open()
+
+    def is_open(self):
+        return self.ssock or self.sock
+
+    def open_secure(self):
+        global USE_SSL
+        if not self.is_open():
+            print("You need to open an insecure connection first!")
+            return
+        elif self.ssock:
+            print("Secure connection already open! - Close it?")
+            return
+        elif not USE_SSL:
+            print("SSL not enabled!")
+            return
+
+        self.scontext = ssl.create_default_context()
+        try:
+            self.ssock = self.scontext.wrap_socket(self.sock, server_hostname=ADDRESS)
+        except ssl.SSLError as e:
+            print("Could not open connection over SSL: " + str(e))
+            self.close()
+            if "WRONG_VERSION_NUMBER" in str(e):
+                print("Turning SSL off and trying again!")
+                USE_SSL = False
+                self.open()
+
+    def open(self):
+        if self.is_open():
+            print("Close connection first!")
+            return
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((ADDRESS, PORT))
+
+        # Check connection establishment
+        reply_code = self.get_response()
+        if reply_code == 421:
+            print("Could not establish a connection!")
+            self.close()
+        elif reply_code == 220:
+            if USE_SSL:
+                self.open_secure()
+        else:
+            print("Unknown connection establishment reply!")
+            self.close()
+
+    def close(self):
+        try:
+            if self.is_open():
+                command = QUIT()
+                success = command.send(self)
+                if not success:
+                    print("Server did not want to close connection - closing on our end anyway - " + str(command.reply_code))
+        except Exception as e:
+            print("Error while trying to close connection - closing on our end anyway - " + str(e))
+
+        if self.ssock:
+            self.ssock.close()
+            self.ssock = None
+        if self.sock:
+            self.sock.close()
+            self.sock = None
+
+    def get_current_socket(self):
+        if not self.is_open():
+            return None
+        current_sock = self.sock
+        if self.ssock:
+            current_sock = self.ssock
+        return current_sock
+
+    def get_response(self):
+        res = self.get_current_socket().recv(1024)
+        print(f"Response: {res}\n")
+        reply_code = int((res.decode("UTF-8"))[:3])
+        return reply_code
+
+    def send(self, req: str):
+        req = req.encode("UTF-8")
+        print(f"Sending: {req}")
+        self.get_current_socket().sendall(req)
+        return self.get_response()
+
+
+class Command:
+    def __init__(self, command: str, reply_codes: dict, content=None):
+        self.command = command
+        self.content = content
+        self.reply_codes = reply_codes
+        self.reply_code = None
+
+    def set_content(self, content=None):
+        self.content = content
+
+    def send(self, conn: Connection):
+        if self.content:
+            self.reply_code = conn.send(" ".join([self.command, self.content]) + "\n")
+        else:
+            self.reply_code = conn.send(self.command + "\n")
+        if self.success(self.reply_code):
+            return True
+        return False
+
+    def success(self, reply_code: int):
+        return "success" in self.reply_codes and reply_code in self.reply_codes["success"]
+
+    def failure(self, reply_code: int):
+        return "failure" in self.reply_codes and reply_code in self.reply_codes["failure"]
+
+    def error(self, reply_code: int):
+        return "error" in self.reply_codes and reply_code in self.reply_codes["error"]
+
+
+class HELO(Command):
+    reply_codes = {
+        "success": [250],
+        "error": [500, 501, 504, 421]
+    }
+
+    def __init__(self, domain=None):
+        super().__init__("HELO", self.reply_codes)
+        super().set_content(domain)
+
+
+class EHLO(Command):
+    reply_codes = {
+        "success": [250],
+        "error": [500, 501, 504, 421]
+    }
+
+    def __init__(self, domain=None):
+        super().__init__("EHLO", self.reply_codes)
+        super().set_content(domain)
+
+
+class MAIL(Command):
+    reply_codes = {
+        "success": [250],
+        "failure": [552, 451, 452],
+        "error": [500, 501, 421]
+    }
+
+    def __init__(self, reverse_path=None):
+        super().__init__("MAIL", self.reply_codes)
+        super().set_content(f"FROM:<{reverse_path}>")
+
+
+class RCPT(Command):
+    reply_codes = {
+        "success": [250, 251],
+        "failure": [550, 551, 552, 553, 450, 451, 452],
+        "error": [500, 501, 503, 421]
+    }
+
+    def __init__(self, forward_path=None):
+        super().__init__("RCPT", self.reply_codes)
+        super().set_content(f"TO:<{forward_path}>")
+
+
+class DATA(Command):
+    reply_codes = {
+        "success": [354],
+        "failure": [451, 554],
+        "error": [500, 501, 503, 421]
+    }
+    reply_codes_2 = {
+        "success": [250],
+        "failure": [552, 554, 451, 452]
+    }
+
+    def __init__(self, content=None):
+        super().__init__("DATA", self.reply_codes)
+        lines = content.split("\n")
+        for i in range(len(lines)):
+            if lines[i].startswith("."):
+                lines[i] = "." + lines[i]  # prevent dot-stuffing
+        self.data = "\n".join(lines)
+
+    def send(self, conn: Connection):
+        # Send initial start
+        if super().send(conn):
+            # Send data
+            self.command = self.data + "\n."
+            self.reply_codes = self.reply_codes_2
+            return super().send(conn)
+
+
+class QUIT(Command):
+    reply_codes = {
+        "success": [221],
+        "error": [500]
+    }
+
+    def __init__(self):
+        super().__init__("QUIT", self.reply_codes)
 
 
 class MIMEContent:
@@ -73,7 +280,6 @@ class File:
     def __new__(cls, *args, **kwargs):
         if "input" in kwargs and kwargs["input"]:
             return super().__new__(cls)
-            
 
     def get_mime_content(self):
         filetype = self.metadata.split(":")[1].split(";")[0]
@@ -93,85 +299,59 @@ class Mail:
         self.content = content
         self.file = File(file)
 
-    def send(self, socket: _socket.socket = None, close_socket: bool = True):
-        send_mail(self, socket, close_socket)
-
-
-def send_mail(mail: Mail, socket: _socket.socket = None, close_socket: bool = True):
-    if not socket:
-        socket = _socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)
-        socket.connect((ADDRESS, PORT))
-    mime_supported = True
-    while True:
-        res = b''
-        res = socket.recv(1024)
-        # print(f"{res!r}")
-        res = res.decode("UTF-8")
-        print(res)
-        req = None
-        if res.startswith("220"):
-            req = "EHLO " + mail.sender.split("@")[1]
-        elif res.startswith("250"):
-            if any(x in res.lower() for x in ["sender", "new message"]):
-                req = "RCPT TO: <" + mail.recipient + ">"
-            elif any(x in res.lower() for x in ["recipient"]):
-                req = "DATA"
-            elif any(x in res.lower() for x in ["accepted for delivery", "mail accepted"]):
-                if close_socket:
-                    req = "QUIT"
-                else:
-                    break
-            else:
-                req = "MAIL FROM: <" + mail.sender + ">"
-        elif res.startswith("354"):
-            header = str("From: " + mail.sender + "\n" +
-                         "To: " + mail.recipient + "\n" +
-                         "Subject: " + mail.subject + "\n")
-            data = mail.content
-            lines = data.split("\n")
-            for i in range(len(lines)):
-                if lines[i].startswith("."):
-                    lines[i] = "." + lines[i]  # prevent dot-stuffing
-            data = "\n".join(lines)
-            if mime_supported:
-                mime_message = MIMEMessage()
-                mime_message.add_body(data)
-
-                if mail.file:
-                    mime_message.add_content(mail.file.get_mime_content())
-
-                req = header+mime_message.get_output()+"."
-            else:
-                req = header+"\n"+data+"\n."
-        elif res.startswith("500"):
-            print("Command unrecognized.")
-            if "EHLO" in res:
+    def send(self, conn, close_connection: bool = True):
+        # ----- Hello and check for MIME support ---- #
+        mime_supported = True
+        command = EHLO(domain=self.sender.split("@")[1])
+        success = command.send(conn)
+        if not success:
+            if command.reply_code == 500:
                 mime_supported = False
-                req = "HELO " + mail.sender.split("@")[1]
+                command = HELO(domain=self.sender.split("@")[1])
+                success = command.send(conn)
+                if not success:
+                    return command.reply_code
             else:
-                break
-        elif res.startswith("503"):
-            print("An error occured.")
-            break
-        elif res.startswith("221"):
-            print("Connection ended.")
-            if not close_socket:
-                raise ConnectionAbortedError("connection ended")
-            break
+                return command.reply_code
 
-        if req:
-            req = req + "\n"
-            print(f"Sending: {(req[:200] + '...') if len(req) > 200 else req}")
-            socket.sendall(req.encode("UTF-8"))
+        # ----- MAIL From ---- #
+        command = MAIL(self.sender)
+        success = command.send(conn)
+        if not success:
+            return command.reply_code
 
-    if close_socket:
-        socket.close()
+        # ----- RCPT To ---- #
+        command = RCPT(self.recipient)
+        success = command.send(conn)
+        if not success:
+            return command.reply_code
+
+        # ----- DATA ---- #
+        header = str("From: " + self.sender + "\n" +
+                     "To: " + self.recipient + "\n" +
+                     "Subject: " + self.subject + "\n")
+        if mime_supported:
+            mime_message = MIMEMessage()
+            mime_message.add_body(self.content)
+
+            if self.file:
+                mime_message.add_content(self.file.get_mime_content())
+
+            data = header + mime_message.get_output() + "."
+        else:
+            data = header + "\n" + self.content + "\n."
+        command = DATA(data)
+        success = command.send(conn)
+        if not success:
+            return command.reply_code
+
+        if close_connection:
+            conn.close()
+
+        return 200
 
 
 def send_mail_raw(sender, recipient, subject, content, file=None):
-    with _socket.socket(_socket.AF_INET, _socket.SOCK_STREAM) as socket:
-        socket.connect((ADDRESS, PORT))
-        mail = Mail(sender, recipient, subject, content, file)
-        mail.send(socket)
-        return 200
-
+    conn = Connection()
+    mail = Mail(sender, recipient, subject, content, file)
+    return mail.send(conn, close_connection=True)
